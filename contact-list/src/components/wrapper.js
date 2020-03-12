9ed6b96d9b6b701e03811c44c0cfaf6ceaf61a05
Karan <k123parmar@gmail.com>
2020-03-12 18:42:39 +0530

    Designed Drawer
    Completed sideMenu aka Drawer

--- contact-list/src/components/wrapper.js
+++ contact-list/src/components/wrapper.js
@@ -1,39 +1,38 @@
 import React from "react";
 import clsx from "clsx";
+import "font-awesome/css/font-awesome.min.css"
 import { makeStyles, useTheme } from "@material-ui/core/styles";
-import Drawer from "@material-ui/core/Drawer";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import List from "@material-ui/core/List";
-import { CssBaseline } from "@material-ui/core";
-import {
-  Twitter,
-  GitHub,
-  Contacts,
-  ClearAllRounded,
-  Translate
-} from "@material-ui/icons";
-import Divider from "@material-ui/core/Divider";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-import ChevronRightIcon from "@material-ui/icons/ChevronRight";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
+import { Drawer, List, Grid, Hidden, AppBar, Toolbar, CssBaseline, Divider, IconButton, ListItem, ListItemIcon, ListItemText, Typography } from "@material-ui/core";
+import { Twitter, GitHub, PersonOutline, ClearAllRounded, ChevronLeft } from "@material-ui/icons";
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
   root: {
-    display: "flex"
+    display: "flex",
+  },
+  active: {
+    boxShadow: "inset 5px 0 0 0 white",
+    background: "rgba(240,240,240,0.1)",
+    color: "rgba(255,255,255,1)"
+  },
+
+  textWhite: {
+    color: "rgba(255,255,255,0.9)"
+  },
+  bgGradient: {
+
+    fontSize: "100px",
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    webkitBackgroundClip: "text",
+    WebkitTextFillColor: "transparent"
   },
   appBar: {
     transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen
-    })
+    }),
+    background: "white"
   },
   appBarShift: {
     marginLeft: drawerWidth,
@@ -44,7 +43,7 @@ const useStyles = makeStyles(theme => ({
     })
   },
   menuButton: {
-    marginRight: 36
+    marginRight: 29
   },
   hide: {
     display: "none"
@@ -52,14 +51,17 @@ const useStyles = makeStyles(theme => ({
   drawer: {
     width: drawerWidth,
     flexShrink: 0,
-    whiteSpace: "nowrap"
+    whiteSpace: "nowrap",
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
   },
+
   drawerOpen: {
     width: drawerWidth,
     transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen
-    })
+    }),
+    overflowX: "hidden"
   },
   drawerClose: {
     transition: theme.transitions.create("width", {
@@ -67,10 +69,11 @@ const useStyles = makeStyles(theme => ({
       duration: theme.transitions.duration.leavingScreen
     }),
     overflowX: "hidden",
-    width: theme.spacing(7) + 1,
-    [theme.breakpoints.up("sm")]: {
-      width: theme.spacing(9) + 1
-    }
+    width: 0,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(8) + 1,
+    },
+
   },
   toolbar: {
     display: "flex",
@@ -82,11 +85,11 @@ const useStyles = makeStyles(theme => ({
   },
   content: {
     flexGrow: 1,
-    padding: theme.spacing(3)
+    padding: theme.spacing(3),
   }
 }));
 
-export default function MiniDrawer() {
+export default function Wrapper() {
   const classes = useStyles();
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
@@ -101,15 +104,7 @@ export default function MiniDrawer() {
 
   return (
     <div className={classes.root}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open
-        })}
-      >
-        <Toolbar style={{ backgroundColor: "white" }} />
-      </AppBar>
+      {/* <CssBaseline /> */}
 
       <Drawer
         variant="permanent"
@@ -118,7 +113,7 @@ export default function MiniDrawer() {
           [classes.drawerClose]: !open
         })}
         classes={{
-          paper: clsx({
+          paper: clsx(classes.drawer, {
             [classes.drawerOpen]: open,
             [classes.drawerClose]: !open
           })
@@ -126,52 +121,84 @@ export default function MiniDrawer() {
       >
         <div className={classes.toolbar}>
           <IconButton onClick={handleDrawerClose}>
-            {theme.direction === "rtl" ? (
-              <ChevronRightIcon />
-            ) : (
-              <ChevronLeftIcon />
-            )}
+            <ChevronLeft fontSize="large" />
           </IconButton>
           <IconButton
             color="inherit"
             aria-label="open drawer"
             onClick={handleDrawerOpen}
             edge="start"
-            className={clsx(classes.menuButton, {
+            className={clsx(classes.menuButton, classes.textWhite, {
               [classes.hide]: open
             })}
-            style={{ left:'50%', transform: "translateX(20%)" }}
           >
-            <ClearAllRounded style={{ fontSize: '150%'}}/>
+            <ClearAllRounded fontSize="large" />
           </IconButton>
-
-          
         </div>
-        <List>
-          <ListItem button>
-            <ListItemIcon>
-              <InboxIcon />
+
+        <List className={classes.textWhite}>
+          <ListItem button className={classes.active} >
+            <ListItemIcon className={classes.textWhite}>
+              <PersonOutline fontSize="large" />
             </ListItemIcon>
             <ListItemText primary="Local" />
           </ListItem>
-          <ListItem button>
-            <ListItemIcon>
-              <Twitter />
+          <ListItem button >
+            <ListItemIcon className={classes.textWhite}>
+              <Twitter fontSize="large" />
             </ListItemIcon>
             <ListItemText primary="Twitter" />
           </ListItem>
           <ListItem button>
-            <ListItemIcon>
-              <GitHub />
+            <ListItemIcon className={classes.textWhite}>
+              <GitHub fontSize="large" />
             </ListItemIcon>
             <ListItemText primary="GitHub" />
           </ListItem>
         </List>
       </Drawer>
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
-        <Contacts /> CONTACTS
-      </main>
+
+      <AppBar style={{ background: "white" }}>
+        <Toolbar>
+          <IconButton
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, {
+              [classes.hide]: open,
+            })}
+          >
+            <ClearAllRounded fontSize="large" />
+          </IconButton>
+
+        </Toolbar></AppBar>
+
+      <Grid container >
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+
+          <Grid container item xs={12} alignItems="center">
+            <Grid item xs={4} sm={1}>
+              <i className="fa fa-address-book fa-flip-horizontal fa-3x icon-gradient" ></i>
+            </Grid>
+
+            <Grid item xs={8} sm={11} className="text-left" style={{ paddingLeft: "5px" }}>
+              <Grid item xs={12}>
+                <Typography variant="h4" component="span">
+                  Contacts </Typography>
+              </Grid>
+
+              <Grid item xs={12}> <Hidden xsDown>
+                <Typography variant="subtitle1" component="span" className="text-silver"> Welcome to InstaConnect</Typography> </Hidden>
+              </Grid>
+
+            </Grid>
+          </Grid>
+
+
+        </main>
+      </Grid>
+
     </div>
   );
 }
